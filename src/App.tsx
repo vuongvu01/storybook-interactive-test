72d9db0c00e53894d7b77f1ecb07233e0930a431
Vuong Vu <vu.vuong@deliveryhero.com>
2022-05-24 11:22:26 +0200

    add  Armor + Storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Button } from "@deliveryhero/armor";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Test</Button>
       </header>
     </div>
   );
